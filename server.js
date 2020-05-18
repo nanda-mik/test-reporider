f21acfc373ff441e50e0b806f894cafb4e4e171f
nanda-mik <nandamikushubha@gmail.com>
2020-05-18 21:56:39 +0530

    commit first

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+const db = require("./config/key").mongoURI;
+
+mongoose.connect(db, {useNewUrlParser: true})
+.then(() => console.log("MongoDb success"))
+.catch(err => console.log(err));
+
+app.use(passport.initialize());
+
+require("./config/passport")(passport);
+
+app.use("/api/users", users);
+
+const port = 5000;
+
+app.listen(port, () => console.log("server up"));
